513fe51e96760456237d624c428792eb71671165
Jan Demmerle <jan.demmerle@why.de>
2025-07-29 21:46:41 +0200

    fix: remove turso auth token from github

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,9 +2,8 @@ import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 
 const turso = createClient({
-  url: 'libsql://grapevine-real-app-eng-why.turso.io',
-  authToken:
-    'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NTMwMzkyNzgsImlkIjoiYjE5ZjgxOTgtODljOC00OGZkLWEzOWMtYjQ4YzE0NTAwNGY3In0.eQ6mbW4vJeWysFr0-y3luXzzvFvH_nRoyPNu4CWUmIGEjA9XacjjSnis96JR4lUW2tdeMFmOFw68jsymsJC7Cg',
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
 });
 
 export const db = drizzle(turso);
