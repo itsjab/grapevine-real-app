2dd45e30b3377c88f5ad59944c3c86d96e6c901b
Jan Demmerle <jan.demmerle@why.de>
2025-08-13 22:21:56 +0200

    fix: wrap login and signup page in suspense

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from 'react';
 import { LoginForm } from '@/components/login-form';
 
 export default function LoginPage() {
   return (
     <div className="w-full bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full md:max-w-3xl">
-        <LoginForm />
+        <Suspense>
+          <LoginForm />
+        </Suspense>
       </div>
     </div>
   );
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from 'react';
 import { SignupForm } from '@/components/signup-form';
 
 export default function SignupPage() {
   return (
     <div className="w-full bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full md:max-w-3xl">
-        <SignupForm />
+        <Suspense>
+          <SignupForm />
+        </Suspense>
       </div>
     </div>
   );
